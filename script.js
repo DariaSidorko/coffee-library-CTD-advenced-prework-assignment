ce0b15524878fd9029014bf12e4b7f879a9125e7
DaraiSidorko <daria.sidorko@gmail.com>
2023-07-21 14:48:01 -0400

    search by drink added

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ let currentCoffeType;
 buildingCards = (json) => {
 
     let cards = document.getElementById("coffe-cards-container");
+
+    if (json.length < 1){
+      const emptySearchMessage = document.createElement("h2");
+      emptySearchMessage.innerText = "No coffe found with provided name! Please enter correct name.";
+      cards.appendChild(emptySearchMessage);
+    }
+
     for(let i=0; i<json.length; i++) {
 
       const card = document.createElement("div");
@@ -61,7 +68,17 @@ buildingCards = (json) => {
       }
 
       // building up new coffee cards either all of filtered based on search input
+      if (searchParam){
+        let filtered = [];
+        for(let i=0; i<json.length; i++){
+          if(json[i].title.toLowerCase().slice(0, searchParam.length) === searchParam.toLowerCase()){
+            filtered.push(json[i])
+          }
+        }
+        buildingCards(filtered)
+      } else {
         buildingCards(json)
+      }
 
     }) 
 
@@ -71,16 +88,34 @@ buildingCards = (json) => {
 document.getElementById("btn-hot").addEventListener("click", function(event){
   event.preventDefault();
 
+  document.getElementById("btn-iced").classList.remove("button-active");
+  document.getElementById("btn-hot").classList.add("button-active");
+
+  document.getElementById("search").classList.remove("show-hide");
+  document.getElementById("search-value").placeholder = "Search HOT coffee drinks"
+
   currentCoffeType = "hot";
   getDrinks(baseUrl, currentCoffeType)
 });
 
 
-document.getElementById("btn-iced").addEventListener("click", function(event){
+  document.getElementById("btn-iced").addEventListener("click", function(event){
   event.preventDefault();
+
+  document.getElementById("btn-hot").classList.remove("button-active");
+  document.getElementById("btn-iced").classList.add("button-active");
+
+  document.getElementById("search").classList.remove("show-hide");
+  document.getElementById("search-value").value = "";
+  document.getElementById("search-value").placeholder = "Search ICED coffee drinks"
 
   currentCoffeType = "iced"
   getDrinks(baseUrl, currentCoffeType)
 });
 
+// func that triggers api call with each letter input into the search form
+search = () => {
+  let value = document.getElementById("search-value").value;
+  getDrinks(baseUrl, currentCoffeType, value)
+}
 
